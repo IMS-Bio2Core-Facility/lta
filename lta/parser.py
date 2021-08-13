204ee65ed042078a5c9cc2a09915b4fcc3946718
rbpatt2019 <rb.patterson.cross@gmail.com>
2021-08-13 16:52:12 +0100

    docs(parser): add new opts to docstring

--- lta/parser.py
+++ lta/parser.py
@@ -15,6 +15,11 @@ The "0" threshold is specified with option ``-t/--threshold``.
 This should be passed as a floating point number between 0 and 1, inclusive.
 If it is not provided,
 then a default of 0.2 is used.
+The number of bootstrap replicates to use for estimating the p-values
+can be specified with ``-b/--boot-reps``.
+Generally, higher repetitions increases accuracy,
+but there is little apparent improvement past ~20000 repetitions
+(the default).
 
 Like all good CLIs,
 ``-V/--version`` returns the version while ``-h/--help`` returns help.
