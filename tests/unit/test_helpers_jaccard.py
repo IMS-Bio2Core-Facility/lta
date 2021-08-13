ee471a4c786ac1e18262c10b214eb532810ab6e2
rbpatt2019 <rb.patterson.cross@gmail.com>
2021-08-13 14:29:48 +0100

    test(unit): add tests for Jaccard
    Provides unit tests for Jaccard module. Frequent use of numpy's assert_allclose to deal with floating points.

---
+++ tests/unit/test_helpers_jaccard.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""Unit tests for the Jaccard module."""
+import numpy as np
+import pytest
+
+import lta.helpers.jaccard as jac
+
+
+def test_sim_2d() -> None:
+    """It raises an index error when input is not 1d."""
+    x = np.array([[True], [False]], bool)
+    y = np.array([[True], [False]], bool)
+    with pytest.raises(IndexError):
+        jac.similarity(x, y)
+
+
+def test_sim_shape() -> None:
+    """It raises an index error when input is not the same length."""
+    x = np.array([True], bool)
+    y = np.array([True, False], bool)
+    with pytest.raises(IndexError):
+        jac.similarity(x, y)
+
+
+def test_sim_bool() -> None:
+    """It raises a TypeError when the dtype is not bool."""
+    x = np.array([1, 0], int)
+    y = np.array([1, 0], int)
+    with pytest.raises(TypeError):
+        jac.similarity(x, y)
+
+
+def test_sim_center() -> None:
+    """It centers the similarity, when center is True."""
+    x = np.array([True, True, False], bool)
+    y = np.array([True, False, True], bool)
+    val = jac.similarity(x, y, center=True)
+    np.testing.assert_allclose(val, -1 / 6)
+
+
+def test_sim_pxpy() -> None:
+    """It uses px/py when passed."""
+    x = np.array([False, False, False], bool)
+    y = np.array([False, False, False], bool)
+    val = jac.similarity(x, y, px=0.5, py=0.5)
+    np.testing.assert_allclose(val, 1 / 3)
+
+
+def test_sim_default() -> None:
+    """It returns the Jaccard similarity."""
+    x = np.array([True, True, False], bool)
+    y = np.array([True, False, True], bool)
+    val = jac.similarity(x, y)
+    np.testing.assert_allclose(val, 1 / 3)
+
+
+def test_distance() -> None:
+    """It calculates the Jaccard distance."""
+    x = np.array([True, True, False], bool)
+    y = np.array([True, False, True], bool)
+    val = jac.distance(x, y)
+    np.testing.assert_allclose(val, 2 / 3)
+
+
+def test_dist_pxpy() -> None:
+    """It uses px/py when passed."""
+    x = np.array([False, False, False], bool)
+    y = np.array([False, False, False], bool)
+    val = jac.distance(x, y, px=0.5, py=0.5)
+    np.testing.assert_allclose(val, 2 / 3)
+
+
+def test_boot_all_ones() -> None:
+    """It raises a RuntimeError if all values are 1."""
+    x = np.array([True, True, True], bool)
+    y = np.array([True, False, True], bool)
+    with pytest.raises(RuntimeError):
+        jac.bootstrap(x, y, n=10)
+
+
+def test_boot_all_zeros() -> None:
+    """It raises a RuntimeError if all values are 0."""
+    x = np.array([True, True, False], bool)
+    y = np.array([False, False, False], bool)
+    with pytest.raises(RuntimeError):
+        jac.bootstrap(x, y, n=10)
+
+
+def test_boot_pxpy() -> None:
+    """It defers to px/py, when passed."""
+    x = np.array([True, True, False], bool)
+    y = np.array([True, False, False], bool)
+    with pytest.raises(RuntimeError):
+        jac.bootstrap(x, y, n=10, px=1, py=1)
+
+
+def test_boot() -> None:
+    """It calculates the Jaccard distance."""
+    x = np.array([True, True, False], bool)
+    y = np.array([True, False, True], bool)
+    val = jac.bootstrap(x, y, n=10)
+    np.testing.assert_allclose(val, (1 / 3, 1))
