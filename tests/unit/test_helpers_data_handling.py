df184bf5f4691349978517accfe7d6fe660d1376
rbpatt2019 <rb.patterson.cross@gmail.com>
2021-08-25 08:59:41 +0100

    test(unit): fix not_zero tests
    These tests now correctly reflect the new parameters and slightly altered usage of this function.

--- tests/unit/test_helpers_data_handling.py
+++ tests/unit/test_helpers_data_handling.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Unit tests for the data_handling module."""
 import sys
-from typing import Callable, List, Tuple
+from typing import Tuple
 
 import pandas as pd
 import pytest
@@ -14,36 +14,14 @@ else:
     from typing_extensions import Literal
 
 
-@pytest.mark.parametrize("axis,shape", [("rows", (3, 2)), ("columns", (2, 3))])
+@pytest.mark.parametrize("axis,shape", [("index", (3, 2)), ("columns", (2, 3))])
 def test_not_zero(
-    binary_df: pd.DataFrame, axis: Literal["rows", "columns"], shape: Tuple[int, int]
+    binary_df: pd.DataFrame, axis: Literal["index", "columns"], shape: Tuple[int, int]
 ) -> None:
     """It correctly groups 0s."""
-    if axis == "rows":
-        df = dh.not_zero(binary_df.transpose(), axis, "y", 0.5)
-    if axis == "columns":
-        df = dh.not_zero(binary_df, axis, "y", 0.5)
-    assert df.shape == shape
-    assert df.index.names == list("xyz")
-    assert df.columns.names == list("xyz")
-
-
-@pytest.mark.parametrize(
-    "axis,drop,rows,columns",
-    [
-        ("rows", ["x", "y"], ["z"], ["x", "y", "z"]),
-        ("columns", ["x", "y"], ["x", "y", "z"], ["z"]),
-    ],
-)
-def test_not_zero_drop(
-    create_df: Callable,
-    axis: Literal["rows", "columns"],
-    drop: List[str],
-    rows: List[str],
-    columns: List[str],
-) -> None:
-    """It correctly drops levels."""
-    df = dh.not_zero(create_df(dims=(6, 6)), axis, "z", 0.2, drop=drop)
-    # Need to think of way to test thresh accuracy
-    assert df.index.names == rows
-    assert df.columns.names == columns
+    if axis == "index":
+        df = dh.not_zero(binary_df.transpose(), axis, "y", "z", thresh=0.5)
+    else:
+        df = dh.not_zero(binary_df, axis, "y", "z", 0.5)
+    assert df.shape == shape, "The shape after filtering is incorrect."
+    assert df.all().all(), "Groups were not correctly detected as not 0."
