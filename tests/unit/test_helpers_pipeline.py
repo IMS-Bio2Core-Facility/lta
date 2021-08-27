5cd62b9958c84d51d696fc32b5fc2a3b6e48c2bf
rbpatt2019 <rb.patterson.cross@gmail.com>
2021-08-27 14:45:33 +0100

    test(unit): support logging
    Use the provide caplog fixture to capture the newly configured logging.

--- tests/unit/test_helpers_pipeline.py
+++ tests/unit/test_helpers_pipeline.py
@@ -8,6 +8,7 @@ only the basic error handling is tested.
 If you can provide any contributions towards better unit tests for data science,
 please reach out!
 """
+import logging
 from pathlib import Path
 
 import pandas as pd
@@ -18,16 +19,20 @@ from pytest_mock import MockerFixture
 from lta.helpers import pipeline
 
 
-def test_raise_NotFound(capsys: capture.CaptureFixture) -> None:
+def test_raise_NotFound(caplog: capture.CaptureFixture) -> None:
     """It handles FileNotFoundErrors."""
     with pytest.raises(FileNotFoundError):
         pipeline.Pipeline(Path("foo.csv"), Path("bar"), "spam", "eggs", "green", 0.2, 1)
-    out, err = capsys.readouterr()
-    assert out == "foo.csv does not exist.\n"
-    assert err == ""
+    assert caplog.record_tuples == [
+        (
+            "lta.helpers.pipeline",
+            logging.ERROR,
+            "foo.csv does not exist. A full traceback follows...",
+        )
+    ], "Log record is not correct."
 
 
-def test_raise_NotDir(mocker: MockerFixture, capsys: capture.CaptureFixture) -> None:
+def test_raise_NotDir(mocker: MockerFixture, caplog: capture.CaptureFixture) -> None:
     """It handles IsADirectoryErrors."""
     # I don't like mocking, but troubleshooting permissions on Github actions is worse
     mocker.patch(
@@ -35,13 +40,27 @@ def test_raise_NotDir(mocker: MockerFixture, capsys: capture.CaptureFixture) -> 
     )
     with pytest.raises(IsADirectoryError):
         pipeline.Pipeline(Path("foo"), Path("bar"), "spam", "eggs", "green", 0.2, 1)
-    out, err = capsys.readouterr()
-    assert out == "foo is a directory.\n"
-    assert err == ""
+    assert caplog.record_tuples == [
+        (
+            "lta.helpers.pipeline",
+            logging.ERROR,
+            "foo is a directory. A full traceback follows...",
+        )
+    ], "Log record is not correct."
 
 
-def test_raise_Runtime(mocker: MockerFixture) -> None:
+def test_raise_Runtime(mocker: MockerFixture, caplog: capture.CaptureFixture) -> None:
     """It fails if a file is empty."""
-    mocker.patch("lta.helpers.data_handling.construct_df", return_value=pd.DataFrame())
-    with pytest.raises(RuntimeError):
-        pipeline.Pipeline(Path("foo"), Path("bar"), "spam", "eggs", "green", 0.2, 1)
+    # I don't like mocking, but troubleshooting permissions on Github actions is worse
+    mocker.patch(
+        "lta.helpers.data_handling.construct_df", side_effect=pd.errors.EmptyDataError
+    )
+    with pytest.raises(pd.errors.EmptyDataError):
+        pipeline.Pipeline(Path("foo.csv"), Path("bar"), "spam", "eggs", "green", 0.2, 1)
+    assert caplog.record_tuples == [
+        (
+            "lta.helpers.pipeline",
+            logging.ERROR,
+            "foo.csv contains no data. A full traceback follows...",
+        )
+    ], "Log record is not correct."
